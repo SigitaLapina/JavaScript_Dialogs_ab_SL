4993eb6c0f035c5d06483f7fbc054a6a30aa49c5
sigitalapina <lapina.sigita2210@gmail.com>
2022-06-13 20:34:12 +0300

    Class09

---
+++ Class_09_20221306/js/jscript.js
@@ -0,0 +1,13 @@
+/*
+radiusNumber = Riņķa linija
+PI
+C =  riņķa līnijas garums
+*/
+
+var radiusNumber = 6371;
+PI = 3.14;
+C = 2*PI*radiusNumber;
+var num = C
+
+document.getElementById("radiusNumber").innerHTML = radiusNumber;
+document.getElementById("num").innerHTML = C;
