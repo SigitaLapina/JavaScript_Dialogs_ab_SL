e51efe4d3f5edfb98cb2fce777e548d386ba4dda
sigitalapina <lapina.sigita2210@gmail.com>
2022-06-15 16:32:43 +0300

    class09md

---
+++ Class_09_20221306/mdscript.js
@@ -0,0 +1,14 @@
+
+function rekina(){
+    // definē mainīgos
+var rezultats;
+var summa;
+var procenti;
+var pazinojums;
+
+
+// nolasa vērtības no lapas -->
+gadi=document.getElementById('gadiInput').value; // 4
+summa=document.getElementById('summaInput').value; // 159
+pazinojums='Nav ievadīti visi dati';
+// 
