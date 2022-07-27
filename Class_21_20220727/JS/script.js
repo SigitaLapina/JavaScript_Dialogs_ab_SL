608e8ba14389f14d7261d0c05d804f4020b7192a
sigitalapina <lapina.sigita2210@gmail.com>
2022-07-27 20:04:59 +0300

    Class_21A

---
+++ Class_21_20220727/JS/script.js
@@ -0,0 +1,28 @@
+/*let features = 'height=600,width=800',
+    url = 'https://www.w3schools.com/js/js_scope.asp';
+
+let jsWindow = window.open(url, 'about', features);
+
+setTimeout(() => {
+    window.open('https://www.w3schools.com/js/js_errors.asp', 'about')
+}, 3000);
+*/
+
+
+let jsWindow = window.open(
+  "http://127.0.0.1:5500/Class_19_20220720/object.html",
+  "about",
+  "height=600,width=800"
+);
+
+setTimeout(() => {
+  window.open("http://127.0.0.1:5500/Class_17_20221307/index.html", "about");
+}, 3000);
+
+setTimeout(() => {
+  jsWindow.resizeTo(600, 300);
+}, 6000);
+
+setTimeout(() => {
+    jsWindow.close();
+  }, 9000);
